2bce34880ead9886b2a884f9fd5a40179a8db8af
firstmentor <jainvikas887@gmail.com>
2020-05-09 16:44:20 +0530

    ff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstpart';
+  title = 'Hi i am Parent';
+
+  students:any[]=[
+    {id:1,name:'vikas',email:'jain@gmail.com',address:'gwalior'},
+    {id:2,name:'viki',email:'jain12@gmail.com',address:'bhind'},
+    {id:3,name:'ram',email:'ram@gmail.com',address:'etawah'},
+    {id:4,name:'raj',email:'jain@gmail.com',address:'delhi'},
+
+
+
+  ];
 }
